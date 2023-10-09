364e3aa799b572403110d890bea2e0626ce488cb
Usaid Huzaifa <usaidhuzaifa321@gmail.com>
2023-10-09 20:14:22 +0500

    second commit

--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -12,12 +12,13 @@ const page = async  () => {
 // console.log(blog)
   return (
     <main className='mt-32'>
-
+            {/*------------ top content -----------------------*/}
         <div className='max-w-2xl w-full space-y-7'>
           <h1 className='text-3xl lg:text-5xl font-bold'> Writing on software design, company building, and the Chemical industry.</h1>
           <p className='text-sm lg:text-base'>All of my long-form thoughts on programming, leadership, product design, and more, collected in chronological order.</p>
         </div>
 
+        {/*-------------- bottom content---------- */}
      <div className='text-sm w-full  lg:flex flex-col lg:border-l lg:max-w-3xl mt-20 mb-36 space-y-16 '>
       {
         blogData.map((items)=>(
@@ -25,7 +26,7 @@ const page = async  () => {
             <section key={items._id}>
               <Link href={`/blogs/${items.slug}`}>
               <div className='lg:flex ml-5'>
-               <div className='border-l pl-2 lg:text-sm text-slate-400  max-w-sm w-72 lg:mt-2'>{`${items.publishedAt}`}</div>
+               <div className='border-l pl-2 lg:text-sm lg:border-l-0 text-slate-400  max-w-sm w-72 lg:mt-2'>{`${items.publishedAt}`}</div>
 
                   <div className=' lg:space-y-4 hover:bg-slate-100/90 rounded-xl w-full border-transparent border-8'>
                   <h2 className='font-medium'>{items.title}</h2>
